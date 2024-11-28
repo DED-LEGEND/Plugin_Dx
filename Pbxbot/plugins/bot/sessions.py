ea787600804d922a07b504f83bf545d32d3cb273
DED-LEGEND <159163158+DED-LEGEND@users.noreply.github.com>
2024-11-28 08:05:02 +0530

    Update sessions.py

--- Pbxbot/plugins/bot/sessions.py
+++ Pbxbot/plugins/bot/sessions.py
@@ -23,7 +23,7 @@ async def session_menu(_, message: Message):
     )
 
 
-@Pbxbot.bot.on_message(filters.regex(r"ɴᴇᴡ 👑") & Config.AUTH_USERS & filters.private)
+@Pbxbot.bot.on_message(filters.regex(r"𝐋𝐮𝐜𝐤𝐲 𝐡𝐨𝐬𝐭 𝐧𝐞𝐰 👑") & Config.AUTH_USERS & filters.private)
 async def new_session(_, message: Message):
     await message.reply_text(
         "**𝖮𝗄𝖺𝗒!** 𝖫𝖾𝗍'𝗌 𝗌𝖾𝗍𝗎𝗉 𝖺 𝗇𝖾𝗐 𝗌𝖾𝗌𝗌𝗂𝗈𝗇",
